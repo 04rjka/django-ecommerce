c4e2b5774a7bf0389fb0a66422daa2b041dd772a
Rahul Joseph <04rjka@gmail.com>
2026-02-04 12:02:21 +0530

    Add functionality Customer Sign Up.

--- ecommerce/core/urls.py
+++ ecommerce/core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("",views.home,name="home"),
+    path("signup/",views.customer_signup,name="customer_signup"),
 ]
--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from .forms import UserForm
 
 def home(request):
     return render(request,"core/home.html")
+
+def customer_signup(request):
+    form = UserForm()
+    if request.method == "POST":
+        username = request.POST.get("username")
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        password = request.POST.get("password")
+        email = request.POST.get("email")
+        if User.objects.filter(email=email).exists():
+            print(email,"Already Exists.")
+            redirect("customer_signup")
+        user = User.objects.create_user(username=username,email=email,first_name=first_name,last_name=last_name,password=password)
+        user.save()
+    return render(request,"core/signup.html",{"form":form})
