e1c24b81d578ee2f0af9a15fbde2b5028cfea2af
Rahul Joseph <04rjka@gmail.com>
2026-03-12 23:19:35 +0530

    Add functionality to view order details.

--- ecommerce/core/urls.py
+++ ecommerce/core/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("delete-address/<int:pk>/",views.delete_address,name="delete_address"),
     path("order-success/<int:pk>/",views.order_success,name="order_success"),
     path("orders/",views.orders,name="orders"),
+    path("order-details/<int:pk>/",views.order_details,name="order_details"),
 ]
--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -212,3 +212,8 @@ def orders(request):
     orders = Order.objects.filter(user=request.user).prefetch_related("orderitems__product")
     # order_items = OrderItem.objects.filter(order=order).select_related("product")
     return render(request,"core/orders.html",{"orders":orders})
+
+def order_details(request,pk):
+    order = Order.objects.get(pk = pk,user=request.user)
+    order_items = OrderItem.objects.filter(order=order).select_related("product")
+    return render(request,"core/order_details.html",{"order":order,"order_items":order_items})
