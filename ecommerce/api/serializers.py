b893ac57d38ccb71230eb43b0b9ecc4066285687
Rahul Joseph <04rjka@gmail.com>
2026-03-25 16:31:32 +0530

    Add serializers for Product and ProductImage. Add api view to return product details and image urls.

--- ecommerce/api/serializers.py
+++ ecommerce/api/serializers.py
@@ -1,1 +1,14 @@
 from rest_framework import serializers
+from core.models import Product,ProductImage
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ["id","image"]
+
+class ProductSerializer(serializers.ModelSerializer):
+    images = ProductImageSerializer(many=True,read_only=True)
+
+    class Meta:
+        model = Product
+        fields = "__all__"
--- ecommerce/api/views.py
+++ ecommerce/api/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from core.models import Product
+from .serializers import ProductSerializer
+
+@api_view(["GET"])
+def products(request):
+    products = Product.objects.filter().prefetch_related('images')
+    serializer = ProductSerializer(products,many=True,context={"request":request})
+    return Response(serializer.data)
