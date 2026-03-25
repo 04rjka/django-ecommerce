ac73350e60a34fa49b52af3804186dcc56a05802
Rahul Joseph <04rjka@gmail.com>
2026-03-25 16:07:40 +0530

    Setup Cors Headers and DRF. Add app api and setup urls.py and serializers.py.

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -46,9 +46,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "core",
+    "corsheaders",
+    "rest_framework",
+    "api"
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -139,3 +144,7 @@ CSRF_TRUSTED_ORIGINS = ['https://*.trycloudflare.com']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 LOGIN_URL = "customer_login"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
