0cd7aff2bde875262f2f9c44eddc8aad835c8072
Rahul Joseph <04rjka@gmail.com>
2026-03-25 22:54:13 +0530

    Fixes for Card.

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 
-export default function Card({name,price,src}) {
+export default function Card({ name, price, src }) {
   return (
-    <>
-        <div className="w-40 sm:w-60 md:w-75 flex flex-col border rounded border-slate-600 bg-white md:flex-row items-center p-4 max-w-sm mx-auto gap-4 hover:shadow-md">
-        <div className="shrink-0">
-            <img className="h-24 w-24 object-cover rounded" src={src} alt=""/>
-        </div>
-        <div>
-            <p className="text-lg font-medium text-slate-800">{name}</p>
-            <p className="font-normal text-slate-500">Rs : {price}</p>
-        </div>
+    <div className="flex flex-col border rounded border-slate-600 bg-white items-center p-4 gap-4 hover:shadow-md">
+      <div className="shrink-0">
+        <img className="h-24 w-24 object-cover rounded" src={src} alt={name} />
+      </div>
+      <div>
+        <p className="text-lg font-medium text-slate-800">{name}</p>
+        <p className="font-normal text-slate-500">Rs : {price}</p>
+      </div>
     </div>
-    </>
   )
 }
