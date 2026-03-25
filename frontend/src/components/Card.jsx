d7430581fa46fbd9f020229676afb5dcc6c5ed27
Rahul Joseph <04rjka@gmail.com>
2026-03-25 22:48:05 +0530

    Setup the Home page and displays the products.

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card({name,price,src}) {
   return (
-    <div>Card</div>
+    <>
+        <div className="w-40 sm:w-60 md:w-75 flex flex-col border rounded border-slate-600 bg-white md:flex-row items-center p-4 max-w-sm mx-auto gap-4 hover:shadow-md">
+        <div className="shrink-0">
+            <img className="h-24 w-24 object-cover rounded" src={src} alt=""/>
+        </div>
+        <div>
+            <p className="text-lg font-medium text-slate-800">{name}</p>
+            <p className="font-normal text-slate-500">Rs : {price}</p>
+        </div>
+    </div>
+    </>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+import Card from '../components/Card'
+import axios from "axios"
+
+
 
 export default function Home() {
+  const [data,setData] = useState([])
+  const fetchData = async ()=> {
+  const response = await axios.get("http://127.0.0.1:8000/api/products/")
+  setData(response.data)
+  }
+
+  useEffect(()=>{
+    fetchData()
+  },[])
   return (
-    <div>Home</div>
+    <div className='grid grid-cols-2 sm:grid-cols-3 p-2 gap-2'>
+      {data.map(product =>(
+        <Card key={product.id} name={product.name} price={product.price} src={product.images[0]?.image}/>
+      ))}
+    </div>
   )
 }
