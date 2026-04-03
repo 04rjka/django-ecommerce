a797d913a6349d52b7d219cb34d186dc4191305d
Rahul Joseph <04rjka@gmail.com>
2026-04-03 23:42:17 +0530

    Add custom hook useFetch. Use useFetch in Homepage and Product Page.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,17 +3,17 @@ import { useState,useEffect } from 'react'
 import Card from '../components/Card'
 import axios from "axios"
 import MainNavbar from '../components/MainNavbar'
+import useFetch from '../hooks/useFetch'
+import PageLoader from '../components/PageLoader'
 
 export default function Home() {
-  const [data,setData] = useState([])
-  const fetchData = async ()=> {
-  const response = await axios.get("http://127.0.0.1:8000/api/products/")
-  setData(response.data)
-  }
 
-  useEffect(()=>{
-    fetchData()
-  },[])
+  const {data,loading,error} = useFetch("http://127.0.0.1:8000/api/products/")
+
+  if(loading) return <PageLoader/>
+
+  if(error) return <div>ERROR</div>
+
   return (
     <div className='grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 p-2 gap-2 bg h-screen'>
       {data.map(product =>(
--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,31 +1,26 @@
-import axios from 'axios'
 import React from 'react'
-import { useState,useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import PageLoader from '../components/PageLoader'
+import useFetch from '../hooks/useFetch'
 
 export default function ProductPage() {
-    const [product,setProductData] = useState(null)
+
     const {id} = useParams()
-    const fetchProductData = async ()=> {
-        const response = await axios.get(`http://127.0.0.1:8000/api/product/${id}/`)
-        setProductData(response.data)
-    }
-    useEffect(()=>{
-        fetchProductData()
-    },[])
-    if (!product)
-        return <PageLoader/>
+    const {data,loading,error} = useFetch(`http://127.0.0.1:8000/api/product/${id}/`)
+
+    if (loading) return <PageLoader/>
+
+    if(error) return <div>ERROR</div>
         
   return (
     <div className='flex flex-col md:flex-row mx-auto justify-center gap-5 p-3'>
         <div className='overflow-hidden  rounded-md shadow-lg'>
-            <img className='md:h-96 w-full object-cover rounded-md' src={product.images[0]?.image} alt="" />
+            <img className='md:h-96 w-full object-cover rounded-md' src={data.images[0]?.image} alt={data.name} />
         </div>
         <div className='flex flex-col items-start  rounded-md p-5 gap-1 md:min-w-60 bg-linear-to-b from-gray-200 to-gray-300 shadow-lg inset-shadow-sm inset-shadow-gray-50'>
-            <p className='text-lg font-semibold'>{product.name}</p>
-            <p className='text-md'>{product.info}</p>
-            <p className='font-bold'>Rs : {product.price}</p>
+            <p className='text-lg font-semibold'>{data.name}</p>
+            <p className='text-md'>{data.info}</p>
+            <p className='font-bold'>Rs : {data.price}</p>
             <div className='flex flex-col gap-1 mt-2 md:justify-center w-full'>
                 <button className='bg-orange-400 p-2 rounded-md font-semibold w-full md:w-auto'>ADD TO CART</button>
                 <button className='bg-yellow-400 p-2 rounded-md font-semibold w-full md:w-auto'>BUY NOW</button>
