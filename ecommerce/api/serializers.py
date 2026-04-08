25c9f532cb1061fe69034aa25a01ee6c71306f56
Rahul Joseph <04rjka@gmail.com>
2026-04-08 12:02:15 +0530

    Updated product serializer to add variants.

--- ecommerce/api/serializers.py
+++ ecommerce/api/serializers.py
@@ -1,20 +1,33 @@
 from rest_framework import serializers
-from core.models import Product,ProductImage,ProductReview
+from core.models import Product,ProductImage,ProductReview,ProductVariant
 
 class ProductImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductImage
         fields = ["id","image"]
 
-class ProductSerializer(serializers.ModelSerializer):
-    images = ProductImageSerializer(many=True,read_only=True)
-
-    class Meta:
-        model = Product
-        fields = "__all__"
-
 class ProductReviewSerializer(serializers.ModelSerializer):
     user = serializers.CharField(source="user.username")
     class Meta:
         model = ProductReview
         fields = ["id","user","title","content","created_at"]
+
+class ProductVariantSerializer(serializers.ModelSerializer):
+    price = serializers.SerializerMethodField()
+    images = ProductImageSerializer(many=True,read_only=True)
+    
+    class Meta:
+        model = ProductVariant
+        fields = ["id","name","stock","price","is_available" ,"images"]
+
+    def get_price(self,obj):
+        return obj.get_price()
+        
+
+class ProductSerializer(serializers.ModelSerializer):
+    images = ProductImageSerializer(many=True,read_only=True)
+    variants = ProductVariantSerializer(many=True,read_only=True)
+
+    class Meta:
+        model = Product
+        fields = "__all__"
